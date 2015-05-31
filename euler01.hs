fcd74f157a2fbe2df78996acdd8dfb1e76dbdfad
David Landry <davidlandry93@gmail.com>
2015-05-31 10:18:43 -0400

    Changed the syntax a little

--- euler01.hs
+++ euler01.hs
@@ -1,7 +1,6 @@
 
 -- Euler 01: My first haskell program
-isFizzBuzz n = n `mod` 3 == 0 || n `mod` 5 == 0
-
 fizzbuzz n = foldr (+) 0 (filter isFizzBuzz [0..n]) 
+    where isFizzBuzz n = n `mod` 3 == 0 || n `mod` 5 == 0
 
 main = putStrLn (show (fizzbuzz 999))
